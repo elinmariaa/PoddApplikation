141cf6e2a9f424d96078d6b42457289a916a55c8
elinmariaa <elinmariasundell@gmail.com>
2025-11-17 08:57:03 +0100

    byggt basklasser

---
+++ PoddApplikation.UI/Models/Klasser/Kategori.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Klasser
+{
+    public class Kategori
+    {
+        public string Id { get; set; }
+        public String Namn { get; set; }
+
+    }
+}
---
+++ PoddApplikation.UI/Models/Undantag/RssHämtningMisslyckades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Undantag
+{
+    public class RssHämtningMisslyckades : Exception
+    {
+        public RssHämtningMisslyckades(string message) : base(message) { }
+
+    }
+}
